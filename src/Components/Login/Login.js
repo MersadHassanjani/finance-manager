96af1ddb4c037dda2a062a67684830e062f0219b
Emiut <emiut97@gmail.com>
2021-07-09 02:31:29 +0430

    login ui changed

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "animate.css";
 import "./Login.css";
 // import { Container } from "react-bootstrap";
 
@@ -6,8 +7,9 @@ const Login = ({ setLoginStatus }) => {
   return (
     <div className="container">
       {/* <div className="col-2"></div> */}
+      <h1 className="loginH1 farsiestb">به "مدیر مالی من" خوش آمدید!</h1>
       <form className="loginform">
-        <h3 className="loginH3 farsiest">ورود به حساب کاربری</h3>
+        <h3 className="loginH3 farsiest ">ورود به حساب کاربری</h3>
 
         <div className="form-group loginInputGroup">
           <label className="loginLabel farsiest">ایمیل</label>
@@ -29,10 +31,12 @@ const Login = ({ setLoginStatus }) => {
 
         <button
           type="submit"
-          className="btn btn-dark btn-lg btn-block farsiest loginBtn"
+          className="btn btn-dark btn-lg btn-block farsiest loginBtn
+          animate__animated animate__pulse animate__infinite animate__slower "
           onClick={(e) => {
             e.preventDefault();
             console.log("loginBtn clocked");
+            alert("ggg");
             setLoginStatus("token2");
           }}
         >
